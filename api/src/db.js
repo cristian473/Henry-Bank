7c7fccc958e2b4614e9548719e35a0976f1d41d5
Mauricio <mauricioarizaga@hotmail.com>
2020-07-24 10:49:24 -0300

    Modelo Users

--- api/src/db.js
+++ api/src/db.js
@@ -1,8 +1,8 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
 function db() {
-  return new Sequelize('postgres://henry:soyhenry@localhost/development', {
-    logging: false, // set to console.log to see the raw SQL queries
+  return new Sequelize("postgres://postgres:postgres@localhost/henrybank", {
+    logging: console.log, // set to console.log to see the raw SQL queries
     native: true, // lets Sequelize know we can use pg-native for ~30% more speed
   });
 }
---
+++ api/src/models/Users.js
@@ -0,0 +1,70 @@
+const Users = (sequelize, S) => {
+  // defino el modelo
+  const U = sequelize.define("users", {
+    userId: {
+      type: S.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    firstName: {
+      type: S.STRING,
+      allowNull: true,
+    },
+    lastName: {
+      type: S.STRING,
+      allowNull: true,
+    },
+    password: {
+      type: S.STRING,
+      allowNull: true,
+    },
+    email: {
+      type: S.STRING,
+      allowNull: true,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    identification: {
+      type: S.STRING,
+      allowNull: true,
+      unique: true,
+    },
+    birthDate: {
+      type: S.INTEGER,
+      allowNull: true,
+    },
+    address: {
+      type: S.STRING,
+      allowNull: true,
+    },
+    city: {
+      type: S.STRING,
+      allowNull: true,
+    },
+    country: {
+      type: S.STRING,
+      allowNull: true,
+    },
+    status: {
+      type: S.BOOLEAN,
+      defaultValue: false,
+    },
+  });
+
+  U.addHook("beforeCreate", function (Users, options) {
+    let ageCheck = new Date();
+    ageCheck.setFullYear(ageCheck.getFullYear() - 16);
+    let birthDate = new Date(Users.birthDate);
+    if (ageCheck < birthDate) {
+      throw new Error("No esta permitido usuarios menores a 16");
+    }
+  });
+
+  return U;
+};
+
+module.exports = Users;
