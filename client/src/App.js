2100327f31376ff7bd899633843225f3210b0284
Júnior Nogueira <haroldognjunior@gmail.com>
2020-07-28 14:05:02 -0300

    formulario agregar usuario

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,18 @@ import React from 'react';
 import './css/App.css';
 import { Route } from 'wouter';
 import { useDispatch } from 'react-redux';
-
 import AddUserForm from './components/FormularioAltaCliente.jsx';
 import LoginForm from './components/Login.jsx';
 import General from './components/General/index.js'
+import FormularioUsuario from './components/FormularioCrearUsuario.jsx'
+
 function App() {
 
   const dispatch = useDispatch();
   return (
     <div>
       <Route path='/addUser' component={AddUserForm} />
+      <Route path='/addUsuario' component={FormularioUsuario} />
       <Route path='/login' component={LoginForm} />
       <Route path='/general' component={General} />
     </div>
