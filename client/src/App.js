5b02eac2b979a88a00459ef858bf2625d93a26c5
DieG02 <43993554@alejandrovolta.edu.ar>
2020-07-28 16:18:22 -0300

    Home View

--- client/src/App.js
+++ client/src/App.js
@@ -5,14 +5,16 @@ import { useDispatch } from 'react-redux';
 
 import AddUserForm from './components/FormularioAltaCliente.jsx';
 import LoginForm from './components/Login.jsx';
+import Home from './components/Onboarding.jsx';
 
 function App() {
 
   const dispatch = useDispatch();
   return (
     <div>
-      <Route path='/users/new' component={AddUserForm} />
-      <Route path='/users/login' component={LoginForm} />
+      <Route exact path='/' component={Home} />
+      <Route exact path='/users/new' component={AddUserForm} />
+      <Route exact path='/users/login' component={LoginForm} />
     </div>
   );
 }
