b8999556188e9673c19d581eab7e16d50c19f290
Agustin Pagano Zavalia <agustinpaganozavalia@gmail.com>
2020-07-29 13:58:00 -0300

    stylingForms

--- client/src/components/Cliente/FormularioAltaDomicilio.jsx
+++ client/src/components/Cliente/FormularioAltaDomicilio.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { addUsers } from '../../actions/AddUserActions'
 import './CSS/altaCliente.css';
-import header from './images/header.png';
 
 const AddUserForm = props => {
     const initialUserState = {
@@ -32,7 +31,6 @@ const AddUserForm = props => {
     return (
         <div>
             <div id="login">
-                <img src={header} alt="header" />
                 <h2>Alta Cliente</h2>
                 <form onSubmit={event => {
                     event.preventDefault()
@@ -41,15 +39,14 @@ const AddUserForm = props => {
                     setUser(initialUserState)
                 }}
                 >
-                    <div class='input-gruop mb-3'>
-                        <input class='form-control' type="text" name="documentType" placeholder="Tipo de doc" value={user.documentType} onChange={handleInputChange} />
-                        <input class='form-control' type="text" name="documentNumber" placeholder="Número" value={user.documentNumber} onChange={handleInputChange} />
-                        <input class='form-control' type="text" name="name" placeholder="Nombre" value={user.name} onChange={handleInputChange} />
-                        <input class='form-control' type="text" name="lastname" placeholder="Apellido" value={user.lastname} onChange={handleInputChange} />
-                        <p>Fecha de nacimiento</p>
-                        <input class='form-control' type="date" name="birthdate" placeholder="Fecha de nacimiento" value={user.birthdate} onChange={handleInputChange} />
-                    </div>
-                    <button>Continuar</button>
+                    <input type="text" name="phone" placeholder="Teléfono" value={user.phone} onChange={handleInputChange} />
+                    <input type="text" name="street" placeholder="Domicilio calle" value={user.street} onChange={handleInputChange} />
+                    <input type="text" name="houseNumber" placeholder="Número" value={user.houseNumber} onChange={handleInputChange} />
+                    <input type="text" name="city" placeholder="Ciudad" value={user.city} onChange={handleInputChange} />
+                    <input type="text" name="province" placeholder="Provincia" value={user.province} onChange={handleInputChange} />
+                    <input type="text" name="country" placeholder="Pais" value={user.country} onChange={handleInputChange} />
+
+                    <button>Darme de alta</button>
                     <button>Atrás</button>
                 </form>
                 <h4>¿Necesitás ayuda?</h4>
