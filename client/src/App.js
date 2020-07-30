0116e92a6093f88ec10ead1cb9beb801096d2e98
German Cejas <germancejas.1988@gmail.com>
2020-07-30 15:44:31 -0300

    NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './images/logo.svg';
+import { Route } from 'react-router-dom'
 import './css/App.css';
+import RecargarDinero from './components/RecargarDinero'
+import NavBar from './components/NavBar'
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <Route exact path='/' component={NavBar} />
+     <Route exact path='/RecargarDinero' component={RecargarDinero} component={NavBar} />
+   </div>
   );
 }
 
