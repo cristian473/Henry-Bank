278b030dc31bc682759d6fca12cff7651ac100d2
SieghartCL <darkito_pwner@hotmail.com>
2020-07-31 13:50:47 -0400

    Se borró console.log del env-config.js

--- api/src/db.js
+++ api/src/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 
 function db() {
-  return new Sequelize("postgres://postgres:postgres@localhost/henrybank", {
+  return new Sequelize("postgres://postgres:hola1234@localhost/henrybank", {
     logging: console.log, // set to console.log to see the raw SQL queries
     native: true, // lets Sequelize know we can use pg-native for ~30% more speed
   });
