ac61f2318ca53fc54dde76c8bc6839191ca4a20a
DieG02 <43993554@alejandrovolta.edu.ar>
2020-08-01 19:52:24 -0300

    sin pg-native, db henrybank

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import './css/App.css';
 import { Route } from 'wouter';
-import { useDispatch } from 'react-redux';
-import NavBar from './components/NavBar/NavBar'
+
 import AddUserForm from "./components/Cliente/FormularioAltaCliente.jsx";
 import AddUserForm2 from "./components/Cliente/FormularioAltaDomicilio.jsx";
 import LoginForm from './components/Usuario/Login.jsx';
 import Home from './components/Usuario/Onboarding.jsx';
-import General from './components/General/index.js'
 import FormularioUsuario from './components/Usuario/FormularioCrearUsuario.jsx'
 import LoginGoogle from './components/Usuario/LoginGoogle.jsx';
 import Cliente from './components/Cliente/Cliente.jsx';
 import RecargarDinero from './components/RecargarDinero/RecargarDinero'
 
 function App() {
-  const dispatch = useDispatch();
   
   return (
     <div>
@@ -23,9 +20,8 @@ function App() {
       <Route exact path='/users/new2' component={AddUserForm2} />
       <Route exact path='/users/login' component={LoginForm} />
       <Route exact path='/users/new' component={FormularioUsuario} />
-      <Route exact path='/general' component={General} />
       <Route exact path='/logingoogle' component={LoginGoogle} />
-      <Route exact path='/cliente' component={Cliente}/>
+      <Route exact path='/cliente' component={Cliente} />
       <Route exact path='/recargardinero' component={RecargarDinero}  />
     </div>
   );
