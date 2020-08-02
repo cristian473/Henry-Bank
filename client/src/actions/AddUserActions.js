58a648c16c448bd57f01cda267b42d6e2c383857
DieG02 <43993554@alejandrovolta.edu.ar>
2020-08-02 11:49:25 -0300

    redireccionar al login

--- client/src/actions/AddUserActions.js
+++ client/src/actions/AddUserActions.js
@@ -22,6 +22,19 @@ export function addUser(user) {
   };
 }
 
+export function modifyUser(id, user) {
+  return (dispatch) => {
+    axios.put(`http://localhost:3001/users/modify/${id}`, user).then((res) => {
+      if (res.status === 200) {
+        window.location.replace('http://localhost:3000/users/login')
+        return dispatch({ type: MODIFY_USER, payload: res.data });
+      } else {
+        alert("Error en campos");
+      }
+    });
+  };
+}
+
 export function loggin(user) {
   return function(dispatch) {
       return fetch('http://localhost:3001/auth/login', {
@@ -32,19 +45,6 @@ export function loggin(user) {
               method: 'POST',
               body: JSON.stringify(user),
               credentials: 'include'
-              
-export function modifyUser(id, user) {
-  return (dispatch) => {
-    axios.put(`http://localhost:3001/users/modify/${id}`, user).then((res) => {
-      if (res.status === 200) {
-        return dispatch({ type: MODIFY_USER, payload: res.data });
-      } else {
-        alert("Error en campos");
-      }
-    });
-  };
-}
-
           })
           .then((res) => {
               if (res.status === 200) {
