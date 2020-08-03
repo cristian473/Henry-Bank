b3988add1f572c48b4381b4c49a4aa2bf68221d4
Júnior Nogueira <haroldognjunior@gmail.com>
2020-08-03 11:51:05 -0300

    logout ok

--- client/src/components/Cliente/BotonLogout.jsx
+++ client/src/components/Cliente/BotonLogout.jsx
@@ -1,17 +1,33 @@
 import React from 'react'
-import { connect } from 'react-redux'
+import { connect, useDispatch } from 'react-redux'
 import { logout } from '../../actions/UserActions';
 import { IoMdLogOut} from 'react-icons/io';
+import axios from 'axios'
+import {LOGOUT} from '../../constants/userConstants'
 
-function BotonLogout({logout,usuario}){
-    
-    function deslogear(){
-        logout(usuario)
+function BotonLogout({logout,usuario, history}){
+
+    const dispatch = useDispatch()
+
+    function desloguear(){
+       
+        axios.get("http://localhost:3001/auth/logout").then((res) => {
+            if (res.status === 200) {
+             console.log('aquí estamos en logout')
+             dispatch({ type: LOGOUT }); 
+             history.push('/')
+             // return dispatch({ type: LOGOUT });
+
+            } else {
+              alert("No fue posible desloguearse");
+            }
+          });
+
     }
 
     return(
         <div>
-            <IoMdLogOut className="btn" type="button" size={60}  onClick={deslogear} color="red" /> 
+            <IoMdLogOut className="btn" type="button" size={60}  onClick={desloguear} color="red" /> 
            
         </div>
     )
