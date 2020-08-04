81789ae57e2613880fd685577b312f80524358e3
cristian <cristian.sebamorales@gmail.com>
2020-08-04 14:58:53 -0300

    contactos

--- api/src/routes/contacts.js
+++ api/src/routes/contacts.js
@@ -10,29 +10,40 @@ server.get("/:idUser", (req,res) => {
 
     const idUser =  req.params.idUser
     
+    //busco el usuario por id
     Users.findByPk(idUser)
         .then(user => {
             var contacts =  user.contacts
+            //creo un array vacío
             var contactsUser = [];
+               
+            if (contacts)
+            {
+                    for (let i = 0; i < contacts.length; i++) {
+                    
+                    Users.findByPk(contacts[i])
+                        .then(contact => {
+                            
+                            //pusheo en el nuevo array, los contactos que voy encontrando
+                            contactsUser.push(contact);
 
-            contacts.forEach(element => {
-                Users.findByPk(element)
-                .then(contact => {
+                            //en la ultima iteracion devuelvo el array con la info de todos los contactos
+                            if(i==contacts.length-1)
+                                res.status(200).json({contactsUser})
 
-                    //No estoy pudiendo pushear el contacto que encuentro en el array
-                    //que defini mas arriba
-                    contactsUser.push(contact);
-                })
-                
-            });
+                        
 
+                        })
+                }
+            }
             
-
-            if(contactsUser!=={}) res.status(200).send(contactsUser)
-            else res.status(404).json({message: "No se tiene contactos"})
+            else{
+                res.status(404).json({message: "No se tiene contactos aúnnnnnnnn"})
+            }
+            
         })
 
-        .catch(err => res.status(400).json(err));
+        .catch(err => res.status(400).json({err}));
 
 
 })
@@ -43,6 +54,7 @@ server.post('/:idUser/addContact', (req,res)=> {
     const idUser =  req.params.idUser;
     const email = req.body.email;
 
+    //busco usuario y usuario a agregar
     let usuario = Users.findByPk(idUser)
     let agregado = Users.findOne({where: {email: email}})
     
@@ -50,8 +62,10 @@ server.post('/:idUser/addContact', (req,res)=> {
     Promise.all([usuario,agregado])
         .then(users=>{
             var contacts = users[0].contacts;
+            //pusheo en el array de contactos el id del contacto a agregar
             contacts.push(users[1].id)
 
+            //updateo el array con el nuevo array que creé
             Users.update({
                 contacts: contacts
             },{
@@ -70,6 +84,45 @@ server.post('/:idUser/addContact', (req,res)=> {
                     console.log(err)});
         
     
+})
+
+
+
+server.delete('/:idUser/deleteContact', (req,res) => {
+
+    const idUser =  req.params.idUser;
+    const email = req.body.email;
+
+    //busco usuario y usuario a borrar
+
+    let usuario = Users.findByPk(idUser)
+    let deleteado = Users.findOne({where: {email: email}})
+    
+
+    Promise.all([usuario,deleteado])
+        .then(users=>{
+            var contacts = users[0].contacts;
+            //creo un nuevo array donde NO exista el id del usuario que quiero borrar
+            const newContacts = contacts.filter(idUsers =>  idUsers != users[1].id)
+
+            //modifico el array de contactos con el nuevo array que excluyó el id a eliminar
+            Users.update({
+                contacts: newContacts
+            },{
+                returning: true, where: {id: users[0].id}
+            })
+            .then(userUpdated=>{
+                res.status(200).send(userUpdated);
+            })
+
+
+
+
+        })
+
+        .catch(err => { res.status(400).send(err)});
+
+
 })
 
 
