a49b1e4a645b60eb1a85ad88eb90e99794e48054
Mauricio <mauricioarizaga@hotmail.com>
2020-08-06 01:13:00 -0300

    DB que no se gitignorea

--- api/src/db.js
+++ api/src/db.js
@@ -1,10 +1,13 @@
 const Sequelize = require("sequelize");
 
 function db() {
-  return new Sequelize("postgres://postgres:123@localhost/henryBank", {
-    logging: false, // set to console.log to see the raw SQL queries
-    native: true, // lets Sequelize know we can use pg-native for ~30% more speed
-  });
+  return new Sequelize(
+    "postgres://postgres:postgres@localhost:5432/henrybank",
+    {
+      logging: console.log, // set to console.log to see the raw SQL queries
+      native: true, // lets Sequelize know we can use pg-native for ~30% more speed
+    }
+  );
 }
 
 module.exports = db;
