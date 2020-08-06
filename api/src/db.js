982c6ddf7d83b044e125541f7c26370c2d53e05b
cristian <cristian.sebamorales@gmail.com>
2020-08-06 00:18:15 -0300

    resetPassword

--- api/src/db.js
+++ api/src/db.js
@@ -2,7 +2,7 @@ const Sequelize = require("sequelize");
 
 function db() {
   return new Sequelize("postgres://postgres:123@localhost/henryBank", {
-    logging: console.log, // set to console.log to see the raw SQL queries
+    logging: false, // set to console.log to see the raw SQL queries
     native: true, // lets Sequelize know we can use pg-native for ~30% more speed
   });
 }
