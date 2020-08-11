81752869a1988a4920e3b97970929845c0bce1b9
German Cejas <germancejas.1988@gmail.com>
2020-08-11 16:47:11 -0300

    responsive 20%

--- client/src/components/Usuario/LoginForm.jsx
+++ client/src/components/Usuario/LoginForm.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import loginPortada from './images/login.png';
 import './CSS/login.css';
+import Container from 'react-bootstrap/Container'
+import Image from 'react-bootstrap/Image'
 
 export default function LoginForm() {
   const cancelar = function (e) {
     window.location.replace('http://localhost:3000')
  }
   return (
-    <div id="login">
-      <form action="http://localhost:3001/auth/login" method="POST">
-        <img src={loginPortada} alt="loginPortada" />
-        <div className="form-group col-md-12">
+    <Container id="login">
+      <Image id="henrybanklogin" src="https://fotos.subefotos.com/f807c25bc9510155673fc2acf1d82a39o.png" ></Image>
+      <form id="formlogin" action="http://localhost:3001/auth/login" method="POST">
+        <div id="contelogin2" className="form-group col-md-12">
           <h4>Iniciar Sesión</h4>
-          <div className="input-group mb-3">
+          <div className="contelogin3">
             <input
               name="email"
               className="form-control"
               placeholder="E-mail"
               required />
           </div>
-        </div>
         <div className="form-group col-md-12 ">
           <div className="input-group mb-3">
             <input
@@ -38,9 +38,10 @@ export default function LoginForm() {
           <div className="form-group col-md-6 inicio">
             <input type="submit" className="btn btn-outline-dark" value="Iniciar Sesión" />
             <button type="button" className="btn btn-outline-danger" value="Cancelar"  onClick={cancelar} >Cancelar</button>
-          </div>
+          </div> 
+        </div>
         </div>
       </form>
-    </div>
+    </Container>
   )
 }
