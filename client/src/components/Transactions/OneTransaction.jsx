b65f8bb022287aff4d110b987c1857122d9aa942
Mauricio <mauricioarizaga@hotmail.com>
2020-08-11 00:34:37 -0300

    Correciones cambios Componentes Transasctions

--- client/src/components/Transactions/OneTransaction.jsx
+++ client/src/components/Transactions/OneTransaction.jsx
@@ -1,32 +1,38 @@
 import React from "react";
 import "./Transactions.css";
 
-export default function Transaction(transaction, id, moment) {
-  return (
-    !!transaction && (
-      //switch case pdoriamos hacerlo
-      <div className="container">
-        <div className="props">
-          <div className="expenses">
-            <h4>Número Transacción</h4>
-            <h5>{transaction.transactionNumber}</h5>
+export default function Transaction(transaction) {
+  if (
+    usuarioConectado.id == transaction.idSender ||
+    usuarioConectado.id == transaction.idReceiver
+  ) {
+    return (
+      !!transaction && (
+        //switch case pdoriamos hacerlo
+        <div className="container">
+          <div className="props">
+            <div className="">
+              <h4>Número Transacción</h4>
+              <h5>{transaction.transactionNumber}</h5>
+            </div>
+            <div className="">
+              <h4>Valor</h4>
+              <h5>{transaction.value}</h5>
+            </div>
+            <div className="">
+              <h4>Estado</h4>
+              <h5>{transaction.state}</h5>
+            </div>
+            <div className="">
+              <h4>Tipo Transacción</h4>
+              <h5>{transaction.transactions_type}</h5>
+            </div>
           </div>
-          <div className="expenses">
-            <h4>Valor</h4>
-            <h5>{transaction.value}</h5>
-          </div>
-          <div className="expenses">
-            <h4>Tipo Transacción</h4>
-            <h5>{transaction.transactions_type}</h5>
-          </div>
-          <div className="expenses">
-            <h4>Estado</h4>
-            <h5>{transaction.state}</h5>
-          </div>
-          <div className="expenses"></div>
         </div>
-      </div>
-    )
-  );
+      )
+    );
+  } else {
+    return <div> El usuario no tiene movimientos</div>;
+  }
 }
 export default Transaction;
