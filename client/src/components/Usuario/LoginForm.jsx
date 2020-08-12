c18be4e3cb9e0966c8080524831d1848804a37b2
German Cejas <germancejas.1988@gmail.com>
2020-08-12 01:07:59 -0300

    css 40%

--- client/src/components/Usuario/LoginForm.jsx
+++ client/src/components/Usuario/LoginForm.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import './CSS/login.css';
 import Container from 'react-bootstrap/Container'
 import Image from 'react-bootstrap/Image'
+import Button from 'react-bootstrap/Button'
+
 
 export default function LoginForm() {
   const cancelar = function (e) {
@@ -9,18 +11,19 @@ export default function LoginForm() {
  }
   return (
     <Container id="login">
-      <Image id="henrybanklogin" src="https://fotos.subefotos.com/840aedf4a95530be0d3faf7cebe2903co.png" ></Image>
+      <Image id="henrybanklogin" src="https://fotos.subefotos.com/4dea2a192c488a2b0128150b4321ea1do.png" ></Image>
       <form id="formlogin" action="http://localhost:3001/auth/login" method="POST">
-        <div id="contelogin2" className="form-group col-md-12">
-          <div className="contelogin3">
+        <div  className="form-group col-md-12" id="contelogin2">
+          <div className="input-group mb-3 id" id="contelogin3">
             <input
               name="email"
               className="form-control"
               placeholder="E-mail"
               required />
           </div>
+        </div>
         <div className="form-group col-md-12 " id="contelogin4">
-          <div className="input-group mb-3">
+          <div className="input-group mb-3" id="contelogin5">
             <input
               name="password"
               type="password"
@@ -28,18 +31,18 @@ export default function LoginForm() {
               placeholder="Contraseña"
               required />
           </div>
+          <Button className="forgotpass" href="/resetpassword">¿Olvidaste tu contraseña?</Button>
         </div>
         <Image id="ondas" src="https://fotos.subefotos.com/e07505b47575212c24ec5fdb5ffc1cb8o.png" ></Image>
           <div className="form-group col-md-6 inicio">
-            <input type="submit" className="btn btn-outline-dark" value="Iniciar Sesión" />
-            <button type="button" className="btn btn-outline-danger" value="Cancelar"  onClick={cancelar} >Cancelar</button>
+
+             <input type="submit"  className="btn btn-outline-dark" value="Iniciar Sesión" />
+              <button type="button"  className="btn btn-outline-danger" value="Cancelar"  onClick={cancelar} >Cancelar</button>
           </div> 
-        <div class="form-row contenedor">
+          <div class="form-row contenedor">
           <div className="form-group col-md-6 link" >
-            <a href="/resetpassword">¿Olvidaste tu contraseña?</a>
-            <a href="/">¿Necesitas ayuda?</a>
+            <a id="ayudaa" href="/">¿Necesitas ayuda?</a>
           </div>
-        </div>
         </div>
       </form>
     </Container>
